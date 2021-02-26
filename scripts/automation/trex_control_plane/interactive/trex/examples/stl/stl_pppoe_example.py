6b8b06b4118d41b78d68c31135dc838e97aece48
Rubens Figueiredo <rubens.figueiredo@bisdn.de>
2021-02-26 15:40:52 +0100

    promisc

--- scripts/automation/trex_control_plane/interactive/trex/examples/stl/stl_pppoe_example.py
+++ scripts/automation/trex_control_plane/interactive/trex/examples/stl/stl_pppoe_example.py
@@ -36,6 +36,7 @@ class DHCPTest(object):
             self.c.reset(ports = self.port) # Force acquire ports, stop the traffic, remove all streams and clear stats
             self.ctx  = self.c.create_service_ctx(port = self.port)
             self.c.set_service_mode(ports = self.port, enabled = True) # enables service mode on port = Rx packets not ignored
+            self.c.set_port_attr(self.port, promiscuous = True)
             self.capture_id = self.c.start_capture(rx_ports = 0, mode = 'fixed')
             
             # create clients
