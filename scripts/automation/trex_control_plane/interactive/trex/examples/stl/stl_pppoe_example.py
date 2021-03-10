74f196a72b2c62387d778cb084df378d4bdd94de
Rubens Figueiredo <rubens.figueiredo@bisdn.de>
2021-03-10 12:25:34 +0100

    add packet payload

--- scripts/automation/trex_control_plane/interactive/trex/examples/stl/stl_pppoe_example.py
+++ scripts/automation/trex_control_plane/interactive/trex/examples/stl/stl_pppoe_example.py
@@ -93,6 +93,9 @@ class PPPoETest(object):
         )
 
         streams = []
+        data = ('32 ff 00 58 00 00 00 01 28 db 00 00 45 00 00 54 00 00 40 00 40 00 5e a5 ca 0b 28 9e c0 a8 28 b2 08 00 be e7 00 00 28 7b 04 11 20 4b f4 3d 0d 00 08 09 0a 0b 0c 0d 0e 0f 10 11 12 13 14 15 16 17 18 19 1a 1b 1c 1d 1e 1f 20 21 22 23 24 25 26 27 28 29 2a 2b 2c 2d 2e 2f 30 31 32 33 34 35 36 37')
+        data_s = ''.join(data.split()).decode('hex')
+
         for client in clients:
             record = client.get_record()
             base_pkt = (
@@ -102,11 +105,12 @@ class PPPoETest(object):
                 / PPPoE(sessionid=record.sid)
                 / PPP(proto="Internet Protocol version 4")
                 / IP(src=record.client_ip, dst="8.8.8.8")
-                / UDP()
+                / UDP(load_data_s)
             )
             pkt = STLPktBuilder(pkt=base_pkt, vm=[])
 
             streams.append(STLStream(packet=pkt, mode=STLTXCont(pps=1000)))
+            print(len(base_pkt))
 
         self.c.add_streams(ports=self.port, streams=streams)
         # self.c.start(ports = self.port, mult = '100%', synchronized=True)
